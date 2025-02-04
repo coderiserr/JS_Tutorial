9708cbfdc07043aff97c5f00f40aab6c085771e3
Abhishek <abhishekk25407@gmail.com>
2025-02-04 19:38:27 +0530

    update

---
+++ typeConversion.js
@@ -0,0 +1,19 @@
+//
+let number1=1
+// let StringNumber1= String( number1);//TYPE OF OBJECT
+let numberBoolean= Boolean( typeof number1);//1-0//true
+console.log(numberBoolean);//null
+
+// let String="33";
+// let numberString= Number(  String);//33//typeof nan
+// let booleanString= Boolean( typeof String);//TRUE
+// console.log(booleanString);//0
+
+// let boolean=true/false
+// let booleanNmuber= Number( boolean);//1-0//typeof-NaN
+// let booleanString= String( boolean);//true-false//typeof boolean
+// console.log(booleanNmuber);
+
+
+
+
