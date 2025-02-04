c98e6b1c7959f8b3490a1ce041a2b4f03c2f47ad
Abhishek <abhishekk25407@gmail.com>
2025-02-04 20:25:58 +0530

    update

--- typeConversion.js
+++ typeConversion.js
@@ -2,7 +2,7 @@
 let number1=1
 // let StringNumber1= String( number1);//TYPE OF OBJECT
 let numberBoolean= Boolean( typeof number1);//1-0//true
-console.log(numberBoolean);//null
+// console.log(numberBoolean);//null
 
 // let String="33";
 // let numberString= Number(  String);//33//typeof nan
@@ -13,6 +13,42 @@ console.log(numberBoolean);//null
 // let booleanNmuber= Number( boolean);//1-0//typeof-NaN
 // let booleanString= String( boolean);//true-false//typeof boolean
 // console.log(booleanNmuber);
+
+//////////////////////////////////////////*********operation******* /////////////////////////////////////
+// console.log(2+2) 
+// console.log(2-1)
+// console.log(2*9)
+// console.log(2**4)
+// console.log(2/6)
+// console.log(2%1)
+
+
+
+
+// console.log("1"+1)//11
+// console.log(1+1)//2
+// console.log("1"+1+5)//115
+// console.log(2+1+"1")//31
+
+
+
+// console.log(+true)//1
+// console.log(-true)//-1
+// console.log(+"")//0
+
+
+// let isNumber=100;
+// console.log(isNumber++)
+
+// console.log(++isNumber);
+
+
+
+
+
+
+
+
 
 
 
